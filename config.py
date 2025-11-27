1cd2daae53ac0a429a6a284e40bd28a3d18f00fb
honzenz <71981115+honzenz@users.noreply.github.com>
2025-11-28 00:25:09 +0300

    Delete config.py

--- config.py
+++
@@ -1,3 +0,0 @@
-BOT_TOKEN = '8553108027:AAH3o2q4Ymoa9kzGsIdJkQjAs-qxEQ4H_eI'  # Вставь сюда токен своего Telegram-бота
-ADMIN_ID = 48583093 # Вставь сюда свой Telegram user_id 
-CRYPTO_PAY_TOKEN = '493176:AAkrR1xC8Gn3FIZlBFX9skRupboBx2BXqhe'  # Вставь сюда свой Crypto Pay API Token 
